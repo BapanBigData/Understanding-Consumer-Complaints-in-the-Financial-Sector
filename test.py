04d1294e1cf7586e8bcb0fddb2cc261fd4c83a28
BapanBigData <bapanmldl7892@gmail.com>
2024-09-21 01:03:34 +0530

    updated

--- test.py
+++ test.py
@@ -55,20 +55,4 @@ spark.stop()
 
 print("Connection closed!!")
 
-import os
-from dotenv import load_dotenv # type: ignore
-
-# loading all the env variables from .env file
-load_dotenv()
-
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-MONGO_DB_URL = os.getenv("MONGO_DB_URL")
-S3_MODEL_DIR_KEY = os.getenv("S3_MODEL_DIR_KEY")
-S3_MODEL_BUCKET_NAME = os.getenv("S3_MODEL_BUCKET_NAME")
-MODEL_SAVED_DIR = os.getenv("MODEL_SAVED_DIR")
-
-print(MODEL_SAVED_DIR)
-
-print("All env variables are working!!")
 
